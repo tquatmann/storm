bd508aeb8ddbd16b068be1c27aee9f83e1f9167b
Peter Lindner <peter.lindner.sbg@gmail.com>
2025-06-16 22:01:25 +0200

    implemented building the quotient of an IDTMC + fixed order of intervals + added tiny test case

--- src/storm/storage/bisimulation/DeterministicIntervalModelBisimulationDecomposition.cpp
+++ src/storm/storage/bisimulation/DeterministicIntervalModelBisimulationDecomposition.cpp
@@ -37,8 +37,130 @@ DeterministicIntervalModelBisimulationDecomposition<ModelType>::DeterministicInt
 
 template<typename ModelType>
 void DeterministicIntervalModelBisimulationDecomposition<ModelType>::buildQuotient() {
-    std::cout << "Size of partition: " << this->partition.getNumberOfBlocks();
-    // TODO: implement actual quotient model construction
+    std::cout << "Size of partition: " << this->partition.getNumberOfBlocks() << std::endl;
+
+    // In order to create the quotient model, we need to construct
+    // (a) the new transition matrix,
+    // (b) the new labeling,
+    // (c) the new reward structures.
+
+    // Prepare a matrix builder for (a).
+    storm::storage::SparseMatrixBuilder<ValueType> builder(this->partition.getNumberOfBlocks(), this->partition.getNumberOfBlocks());
+
+    // Prepare the new state labeling for (b).
+    storm::models::sparse::StateLabeling newLabeling(this->partition.getNumberOfBlocks());
+    std::set<std::string> atomicPropositionsSet = this->options.respectedAtomicPropositions.get();
+    atomicPropositionsSet.insert("init");
+    std::vector<std::string> atomicPropositions = std::vector<std::string>(atomicPropositionsSet.begin(), atomicPropositionsSet.end());
+    for (auto const& ap : atomicPropositions) {
+        newLabeling.addLabel(ap);
+    }
+
+    // If the model had state rewards, we need to build the state rewards for the quotient as well.
+    std::optional<std::vector<ValueType>> stateRewards;
+    if (this->options.getKeepRewards() && this->model.hasRewardModel()) {
+        stateRewards = std::vector<ValueType>(this->partition.getNumberOfBlocks());
+    }
+
+    // Now build (a) and (b) by traversing all blocks.
+
+    // TODO: create mapping from representative state to unique identifier
+    std::map<uint64_t, uint64_t> blocksMapping;
+
+    auto blockIndex = 0;
+    this->partition.forEachBlock([&](auto const& block) {
+        blocksMapping.emplace(block.front(), blockIndex);
+        blockIndex++;
+    });
+
+    this->partition.forEachBlock([&](auto const& block) {
+        // Pick one representative state. For strong bisimulation it doesn't matter which state it is, because
+        // they all behave equally.
+        auto representativeState = block.front();
+
+        // TODO: Handle weak bisimulation case (non-silent)
+
+        blockIndex = blocksMapping.at(representativeState);
+
+        // If the block is absorbing, we simply add a self-loop.
+        if (this->absorbingBlocks.contains(representativeState)) {
+            builder.addNextValue(blockIndex, blockIndex, storm::utility::one<ValueType>());
+
+            // If the block has a special representative state, we retrieve it now.
+            representativeState = this->absorbingBlocks.at(representativeState);
+
+            // Add all selected atomic propositions that hold in the representative state to the state
+            // representing the block.
+            for (auto const& ap : atomicPropositions) {
+                if (this->model.getStateLabeling().getStateHasLabel(ap, representativeState)) {
+                    newLabeling.addLabelToState(ap, blockIndex);
+                }
+            }
+        } else {
+            // Compute the outgoing transitions of the block.
+            std::map<storm::storage::sparse::state_type, ValueType> blockProbability;
+            for (auto const& entry : this->model.getTransitionMatrix().getRow(representativeState)) {
+                auto targetBlock = blocksMapping.at(this->partition.getBlockOfElement(entry.getColumn()).front());
+
+                // If we are computing a weak bisimulation quotient, there is no need to add self-loops.
+                // TODO: Extend for weak bisimulation
+
+                auto probIterator = blockProbability.find(targetBlock);
+                if (probIterator != blockProbability.end()) {
+                    probIterator->second += entry.getValue();
+
+                    // Normalize interval
+                    auto normalizedInterval = storm::Interval(std::min(1.0, probIterator->second.lower()), std::min(1.0, probIterator->second.upper()));
+                    probIterator->second = normalizedInterval;
+                } else {
+                    blockProbability[targetBlock] = entry.getValue();
+                }
+            }
+
+            // Now add them to the actual matrix.
+            for (auto const& probabilityEntry : blockProbability) {
+                // TODO: Handle case for weak bisimulation
+                builder.addNextValue(blockIndex, probabilityEntry.first, probabilityEntry.second);
+            }
+
+            // Otherwise add all atomic propositions to the equivalence class that the representative state
+            // satisfies.
+            for (auto const& ap : atomicPropositions) {
+                if (this->model.getStateLabeling().getStateHasLabel(ap, representativeState)) {
+                    newLabeling.addLabelToState(ap, blockIndex);
+                }
+            }
+        }
+
+        // If the model has state rewards, we simply copy the state reward of the representative state, because
+        // all states in a block are guaranteed to have the same state reward.
+        if (this->options.getKeepRewards() && this->model.hasRewardModel()) {
+            auto const& rewardModel = this->model.getUniqueRewardModel();
+            if (rewardModel.hasStateRewards()) {
+                stateRewards.value()[blockIndex] = rewardModel.getStateRewardVector()[representativeState];
+            }
+            if (rewardModel.hasStateActionRewards()) {
+                stateRewards.value()[blockIndex] += rewardModel.getStateActionRewardVector()[representativeState];
+            }
+        }
+    });
+
+    // Now check which of the blocks of the partition contain at least one initial state.
+    for (auto initialState : this->model.getInitialStates()) {
+        auto initialBlock = this->partition.getBlockOfElement(initialState);
+        newLabeling.addLabelToState("init", blocksMapping.at(initialBlock.front()));
+    }
+
+    // Construct the reward model mapping.
+    std::unordered_map<std::string, typename ModelType::RewardModelType> rewardModels;
+    if (this->options.getKeepRewards() && this->model.hasRewardModel()) {
+        STORM_LOG_THROW(this->model.hasUniqueRewardModel(), storm::exceptions::IllegalFunctionCallException, "Cannot preserve more than one reward model.");
+        auto nameRewardModelPair = this->model.getRewardModels().begin();
+        rewardModels.insert(std::make_pair(nameRewardModelPair->first, typename ModelType::RewardModelType(stateRewards)));
+    }
+
+    // Finally construct the quotient model.
+    this->quotient = std::shared_ptr<ModelType>(new ModelType(builder.build(), std::move(newLabeling), std::move(rewardModels)));
 }
 
 template<typename ModelType>
@@ -64,9 +186,7 @@ void DeterministicIntervalModelBisimulationDecomposition<ModelType>::refineParti
     bisimulation::Partition::BlockSet& enqueuedSplitterBlocks) {
     storm::storage::bisimulation::Partition::BlockSet blocksToSplit;
 
-    // std::fill(probabilitiesToCurrentSplitter.begin(), probabilitiesToCurrentSplitter.end(), storm::utility::zero<ValueType>());
     for (auto currentState : splitterBlock) {
-        // Compute probability to enter splitter block for each predecessor
         for (const auto& predecessorEntry : this->backwardTransitions.getRow(currentState)) {
             auto predecessorState = predecessorEntry.getColumn();
             auto predecessorBlock = this->partition.getBlockOfElement(predecessorState);
@@ -76,20 +196,21 @@ void DeterministicIntervalModelBisimulationDecomposition<ModelType>::refineParti
                 continue;
             }
 
-            // TODO: Compute both intervals going to the splitter and going to all other blocks
+            // Compute interval going to the splitter, i.e., I(s, C), and initialize interval leading to all other blocks, i.e., I(s, \Pi \setminus C)
             if (touchedProbabilitiesToSplitter.get(predecessorState)) {
                 probabilitiesToCurrentSplitter[predecessorState] += intervalTransitionProbability;
             } else {
                 probabilitiesToCurrentSplitter[predecessorState] = intervalTransitionProbability;
-                probabilitiesToOtherBlocks[predecessorState] = storm::utility::zero<ValueType>(); // init
+                probabilitiesToOtherBlocks[predecessorState] = storm::utility::zero<ValueType>(); // init I(s, \Pi \setminus C)
                 touchedProbabilitiesToSplitter.set(predecessorState, true);
             }
 
-            // Remember which blocks contain predecessors to split them w.r.t. the splitter afterwards
+            // Remember which blocks contain predecessors to split w.r.t. the splitter afterwards
             blocksToSplit.insert(predecessorBlock);
         }
     }
 
+    // Compute interval to all blocks except the splitter, i.e., I(s, \Pi \setminus C)
     for (auto predecessorState : touchedProbabilitiesToSplitter) {
         for (const auto& entry : this->model.getTransitionMatrix().getRow(predecessorState)) {
             auto targetState = entry.getColumn();
@@ -99,29 +220,6 @@ void DeterministicIntervalModelBisimulationDecomposition<ModelType>::refineParti
             }
         }
     }
-
-    //for (auto predecessorBlockToSplit : blocksToSplit) {
-    //    // First split the block by whether it is a predecessor of the splitter block or not
-    //    bool wasSplit = this->partition.splitBlockByOrder(predecessorBlockToSplit, [this]
-    //                                                      (auto const& a, auto const& b) {
-    //                                                          if (!touchedProbabilitiesToSplitter.get(a) || !touchedProbabilitiesToSplitter.get(b)) {
-    //                                                              std::cout << "Comparing states that were not touched: " << a << ", " << b << std::endl;
-    //                                                              return false;
-    //                                                          }
-    //                                                          return computeIntervalProjection(probabilitiesToCurrentSplitter[a], probabilitiesToOtherBlocks[a])
-    //                                                          < computeIntervalProjection(probabilitiesToCurrentSplitter[b], probabilitiesToOtherBlocks[b]);
-    //                                                      });
-//
-    //    // Add all blocks that were split to splitter queue
-    //    if (wasSplit) {
-    //        this->partition.forEachSubBlock(predecessorBlockToSplit, [&splitterQueue, &enqueuedSplitterBlocks](auto const& block) {
-    //            if (!enqueuedSplitterBlocks.contains(block)) {
-    //                splitterQueue.push_back(block);
-    //                enqueuedSplitterBlocks.insert(block);
-    //            }
-    //        });
-    //    }
-    //}
 
     for (auto predecessorBlockToSplit : blocksToSplit) {
         // First split the block by whether it is a predecessor of the splitter block or not
@@ -144,8 +242,15 @@ void DeterministicIntervalModelBisimulationDecomposition<ModelType>::refineParti
                                                                       return false;
                                                                   }
 
-                                                                  return computeIntervalProjection(probabilitiesToCurrentSplitter[a], probabilitiesToOtherBlocks[a])
-                                                                         < computeIntervalProjection(probabilitiesToCurrentSplitter[b], probabilitiesToOtherBlocks[b]);
+                                                                  auto projectedIntervalOfStateA = computeIntervalProjection(probabilitiesToCurrentSplitter[a], probabilitiesToOtherBlocks[a]);
+                                                                  auto projectedIntervalOfStateB = computeIntervalProjection(probabilitiesToCurrentSplitter[b], probabilitiesToOtherBlocks[b]);
+
+                                                                  // TODO: [0.1, 0.2] < [0.1, 0.3] returns false!
+                                                                  // auto result = projectedIntervalOfStateA < projectedIntervalOfStateB;
+
+                                                                  return projectedIntervalOfStateA.lower() < projectedIntervalOfStateB.lower() ||
+                                                                         (projectedIntervalOfStateA.lower() == projectedIntervalOfStateB.lower() &&
+                                                                          projectedIntervalOfStateA.upper() < projectedIntervalOfStateB.upper());
                                                               });
 
             // Add all blocks that were split to splitter queue
--- src/test/storm/storage/DeterministicIntervalModelBisimulationDecompositionTest.cpp
+++ src/test/storm/storage/DeterministicIntervalModelBisimulationDecompositionTest.cpp
@@ -9,6 +9,8 @@
 #include <vector>
 #include "storm/storage/geometry/Halfspace.h"
 
+#include "storm/modelchecker/prctl/SparseDtmcPrctlModelChecker.h"
+
 #include "storm-config.h"
 #include "storm-parsers/parser/AutoParser.h"
 #include "storm-parsers/parser/FormulaParser.h"
@@ -26,9 +28,42 @@ TEST(DeterministicIntervalModelBisimulationDecompositionTest, CreatePolytopesfro
 
     storm::storage::DeterministicIntervalModelBisimulationDecomposition<storm::models::sparse::Dtmc<storm::Interval>> bisim(*dtmc);
     ASSERT_NO_THROW(bisim.computeBisimulationDecomposition());
-    ASSERT_NO_THROW(bisim.getQuotient());
+    std::shared_ptr<storm::models::sparse::Model<storm::Interval>> result;
+    ASSERT_NO_THROW(result = bisim.getQuotient());
+
+    EXPECT_EQ(storm::models::ModelType::Dtmc, result->getType());
+    EXPECT_EQ(328ul, result->getNumberOfStates());
+    EXPECT_EQ(456ul, result->getNumberOfTransitions());
+
+    // Property on BRP: Pmin=? [F (s = 5)]
 }
 
-// TODO: Create test case for toy example on Remarkable
+// TODO: Create quotient of toy example manually on paper and compare
+TEST(DeterministicIntervalModelBisimulationDecompositionTest, TinyIDTMC) {
+    // TODO: Parser has a bug. It does not check whether the drn-file represents a valid IDTMC.
+    // TODO: Set transition: 4 : [0.1, 0.3] of state 4 to [0.1, 0.2], then there is no possible realization s.t. p_1 + _2 = 1
+
+    std::shared_ptr<storm::models::sparse::Model<storm::Interval>> modelPtr =
+        storm::parser::DirectEncodingParser<storm::Interval>::parseModel(STORM_TEST_RESOURCES_DIR "/idtmc/tiny-01.drn");
+    std::shared_ptr<storm::models::sparse::Dtmc<storm::Interval>> dtmc = modelPtr->as<storm::models::sparse::Dtmc<storm::Interval>>();
+    ASSERT_EQ(storm::models::ModelType::Dtmc, modelPtr->getType());
+    ASSERT_EQ(7ul, dtmc->getNumberOfStates());
+    EXPECT_TRUE(modelPtr->hasUncertainty());
+
+    modelPtr->printModelInformationToStream(std::cout);
+
+    storm::storage::DeterministicIntervalModelBisimulationDecomposition<storm::models::sparse::Dtmc<storm::Interval>> bisim(*dtmc);
+    ASSERT_NO_THROW(bisim.computeBisimulationDecomposition());
+    std::shared_ptr<storm::models::sparse::Model<storm::Interval>> result;
+    ASSERT_NO_THROW(result = bisim.getQuotient());
+
+    EXPECT_EQ(storm::models::ModelType::Dtmc, result->getType());
+    EXPECT_EQ(4ul, result->getNumberOfStates());
+    EXPECT_EQ(6ul, result->getNumberOfTransitions());
+
+    result->printModelInformationToStream(std::cout);
+
+    // Property on BRP: Pmin=? [F (s = 5)]
+}
 
 }
